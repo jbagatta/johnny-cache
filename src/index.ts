8ff5378fc3a407f00a7bd289e6809f380fcd2428
jbagatta <jbagatta@gmail.com>
2023-05-10 19:13:00 -0400

    export ports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './core/types'
+export * from './ports/data-store'
+export * from './ports/message-broker'
 export * from './factory/distributed-dictionary-factory'
 export * from './factory/types'
